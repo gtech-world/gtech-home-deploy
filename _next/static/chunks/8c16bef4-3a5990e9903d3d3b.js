57c12b8a1b954994f196593981c9070c5e8eb44b
XueMoMo <XueMoMo@users.noreply.github.com>
2023-02-23 08:08:58 +0000

    deploy: gtech-world/gtech-home@139d6552ce46be11dbff59565ae0e5cf2d5a9319

---
+++ _next/static/chunks/8c16bef4-3a5990e9903d3d3b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[222],{7191:function(t,n,r){r.d(n,{otZ:function(){return o}});var e=r(7833);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M4 6h16M4 12h16M4 18h16"}}]})(t)}}}]);
